5982a8b0d6debb02c19cf8f623a3266d23b5b25c
yenugulaVamshi <yenugulavamshikrishna16@gmail.com>
2025-06-24 22:51:42 -0400

    Pushing files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def hello_world():
+    # Return an HTML paragraph with "Hello, World!"
+    return "<p>Hello, World!</p>"
+
+# Run the application if the script is executed directly
+if __name__ == "__main__":
+    app.run(debug=True) # debug=True enables debug mode for automatic reloading and error messages
